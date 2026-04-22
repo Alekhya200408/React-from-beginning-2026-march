c8567611b3a3796922daa0ee219c2f5c5ce1d1ac
Alekhya Ghosh <officialalekhya2004@gmail.com>
2026-04-22 08:30:28 +0530

    feat: Implement user authentication context with Login and Profile components

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -3,14 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/profile'
 
 function App() {
   
 
   return (
-    <>
-    <h1>Holaa</h1>
-    </>
+   <UserContextProvider>
+    <h1>Holaa</h1> 
+    <Login/>
+    <Profile/> 
+   </UserContextProvider>
   )
 }
 
